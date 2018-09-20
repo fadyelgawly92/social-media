2e6d13a4efc3b7cc19a60adeca58ec57a3a73c32
fadyelgawly92 <fadyraafatelgawly@gmail.com>
2018-09-20 23:36:23 +0200

    facebook , twitter , github apps with client_id , client_secret, callback_url are created and added in the config/service and .env files

--- config/services.php
+++ config/services.php
@@ -35,4 +35,22 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'twitter' => [
+        'client_id' => env('AIbFhbau61znbecxxDiAJ1wb3'),
+        'client_secret' => env('HioDz1JE5h0ESFdTS5F8yK7xqv0QcadvPoCWDJNHrEUofQT75X'),
+        'redirect' => env('http://localhost:8000/auth/twitter/callback'),
+    ],
+
+    'facebook' => [
+        'client_id' => env('265172104124240'),
+        'client_secret' => env('eba65393ee5242553fb0b3036a701a04'),
+        'redirect' => env('http://localhost:8000/auth/facebook/callback'),
+    ],
+
+    'github' => [
+        'client_id' => env('3e9fce35cd97944a51ce'),
+        'client_secret' => env('8d85bf121815d8f55936ee079e88e3d956179b67'),
+        'redirect' => env('http://localhost:8000/auth/github/callback')
+    ],
+
 ];
